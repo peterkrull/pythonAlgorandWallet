8a6df83f0482740b4077ca9b7ebde81b06dda24b
Peter Krull <peterkrullpeter@gmail.com>
2021-09-30 20:18:48 +0200

    AlgoExplorerAPI node uses proper AlgodClient implementation.

--- AlgoExplorerAPI.py
+++ AlgoExplorerAPI.py
@@ -1,7 +1,8 @@
 import algosdk
+from algosdk.algod import AlgodClient
 
 # AlgoExplorer API compatability class
-class node:
+class node(algosdk.algod.AlgodClient):
 
     # 'requests' library to interact with API
     req = __import__('requests')
@@ -10,18 +11,24 @@ class node:
     def __init__(self,net = "mainnet"):
         self.net = net.lower()
         if net.lower() == "mainnet":
-            self.base_url = "https://algoexplorerapi.io/"
+            self.base_url = "https://algoexplorerapi.io"
         elif net.lower() == "testnet":
-            self.base_url = "https://testnet.algoexplorerapi.io/"
+            self.base_url = "https://testnet.algoexplorerapi.io"
         elif net.lower() == "betanet":
-            self.base_url = "https://betanet.algoexplorerapi.io/"
+            self.base_url = "https://betanet.algoexplorerapi.io"
         else:
             print("Invalid net type : " + str(net) + " ; defaulting to testnet.")
-            self.base_url = "https://testnet.algoexplorerapi.io/"
+            self.base_url = "https://testnet.algoexplorerapi.io"
             self.net = "testnet"
+        
+        
+        # Used for raw API interactions
+        self.algod_token = ""
+        self.algod_address = self.base_url
+        self.headers = {"User-Agent": "API-request"}
     
     # returns relevant explorer URL
-    def explorer(self):
+    def legacy_explorer(self):
         if self.net == "mainnet":
             return "https://algoexplorer.io/"
         elif self.net == "testnet":
@@ -30,13 +37,13 @@ class node:
             return "https://betanet.algoexplorer.io/"
 
     # Returns true if the API is healthy    
-    def health(self):
-        temp_url = self.base_url + str("health")
+    def legacy_health(self):
+        temp_url = self.base_url + str("/health")
         return self.req.get(temp_url).ok
 
     # Gets parameters for new transactions
-    def suggested_params(self):
-        temp_url = self.base_url + str("v2/transactions/params")
+    def legacy_suggested_params(self):
+        temp_url = self.base_url + str("/v2/transactions/params")
         response = self.req.get(temp_url)
         
         if response.ok:
@@ -56,8 +63,8 @@ class node:
             raise Exception("Can't connect to API. URL might be incorrect.")
 
     # Gets the info related to a public address
-    def account_info(self,public_addr):
-        temp_url = self.base_url + str("v2/accounts/" + str(public_addr))
+    def legacy_account_info(self,public_addr):
+        temp_url = self.base_url + str("/v2/accounts/" + str(public_addr))
         response = self.req.get(temp_url)
         if response.ok:
             import ast
@@ -66,8 +73,8 @@ class node:
             raise Exception("API call failed :{}".format(response.content))
     
     # make post transaction to the blockchain using API
-    def send_transaction(self,signed_txn):
+    def legacy_send_transaction(self,signed_txn):
         import base64
         # Turn a Algorand MSGpack into a binary that is compatible with the API
         signed_txn = base64.b64decode(algosdk.encoding.msgpack_encode(signed_txn))
-        return self.req.post(self.base_url + "v2/transactions",signed_txn)
+        return self.req.post(self.base_url + "/v2/transactions",signed_txn)
